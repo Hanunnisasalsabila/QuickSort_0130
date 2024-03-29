161f16dfb2524cb270ecf804afa55185fba1a71c
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2024-03-29 10:38:40 +0700

    add char

--- QuickSort_0130/QuickSort_0130.cpp
+++ QuickSort_0130/QuickSort_0130.cpp
@@ -107,7 +107,20 @@ void display() {
 
 int main()
 {
-    
+    char ch;
+
+    do {
+        input();
+        q_short(0, n - 1);
+        display();
+        cout << "\n\nDo you want to continue? (y/n): ";
+        cin >> ch;
+        if (ch == 'n' || ch == 'N')
+            break;
+
+        system("pause");
+        system("cls");
+    }
 }
 
 
